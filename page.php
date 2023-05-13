4b66f9b653404e33dab6e20b4233a0c5a721b70d
chienvn <chiennguyen1702@gmail.com>
2023-05-13 10:37:29 +0700

    menu of child page link

--- page.php
+++ page.php
@@ -17,24 +17,40 @@
     
     <div class="container container--narrow page-section">
     <?php
-      $parent = wp_get_post_parent_id(get_the_ID());
-       if ($parent) {
+      $theParent = wp_get_post_parent_id(get_the_ID());
+       if ($theParent) {
         ?>
           <div class="metabox metabox--position-up metabox--with-home-link">
             <p>
-              <a class="metabox__blog-home-link" href="<?php echo get_the_permalink($parent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parent) ?></a> <span class="metabox__main"><?php echo the_title(); ?></span>
+              <a class="metabox__blog-home-link" href="<?php echo get_the_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent) ?></a> <span class="metabox__main"><?php echo the_title(); ?></span>
             </p>
           </div>
           <?php
        }
     ?>
-      <!-- <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
-        <ul class="min-list">
-          <li class="current_page_item"><a href="#">Our History</a></li>
-          <li><a href="#">Our Goals</a></li>
-        </ul>
-      </div> -->
+      <?php 
+        $hasChildren = get_pages(array(
+          'child_of' => get_the_ID()
+        ));
+        if ($theParent || $hasChildren) : ?>
+        <div class="page-links">
+          <h2 class="page-links__title"><a href="<?php echo get_the_permalink($theParent) ?>"><?php echo get_the_title($theParent) ?></a></h2>
+          <ul class="min-list">
+            <?php
+              if ($theParent) {
+                $findChildOf = $theParent;
+              } else {
+                $findChildOf = get_the_ID();
+              }
+              wp_list_pages(array(
+                'title_li' => null,
+                'child_of' => $findChildOf
+              ))
+            ?>
+          </ul>
+        </div>
+      <?php endif; ?>
+      
       <div class="generic-content">
         <?php the_content(); ?>
       </div>

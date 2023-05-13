0e64b3169a8473fbef8e3a2d7adff24fab2ef7c3
chienvn <chiennguyen1702@gmail.com>
2023-05-13 08:32:27 +0700

    page and parent page

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-  
+  // Add styles, script to theme
   function university_files() {
     wp_enqueue_script('main-script', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
     wp_enqueue_style('font-google', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
@@ -7,6 +7,11 @@
     wp_enqueue_style('university_main_style', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('university_extra_style', get_theme_file_uri('/build/index.css'));
   }
-  
   add_action('wp_enqueue_scripts', 'university_files');
+  
+  // Change the title
+  function university_features() {
+    add_theme_support('title-tag');
+  }
+  add_action('after_setup_theme', 'university_features');
 ?>

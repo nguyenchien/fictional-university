a08fa2f683f9f0e194a229af10dbbb2d5c50fa12
chienvn <chiennguyen1702@gmail.com>
2023-05-13 17:28:24 +0700

    blog page

--- page.php
+++ page.php
@@ -1,33 +1,30 @@
-<?php
-  get_header();
-?>
+<?php get_header(); ?>
+<div class="page-banner">
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
+  <div class="page-banner__content container container--narrow">
+    <h1 class="page-banner__title"><?php the_title() ?></h1>
+    <div class="page-banner__intro">
+      <p>Don't forget change me later</p>
+    </div>
+  </div>
+</div>
   <?php
     while(have_posts()):
       the_post()
   ?>
-    <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title() ?></h1>
-        <div class="page-banner__intro">
-          <p>Don't forget change me later</p>
-        </div>
-      </div>
-    </div>
-    
     <div class="container container--narrow page-section">
-    <?php
-      $theParent = wp_get_post_parent_id(get_the_ID());
-       if ($theParent) {
-        ?>
-          <div class="metabox metabox--position-up metabox--with-home-link">
-            <p>
-              <a class="metabox__blog-home-link" href="<?php echo get_the_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent) ?></a> <span class="metabox__main"><?php echo the_title(); ?></span>
-            </p>
-          </div>
-          <?php
-       }
-    ?>
+      <?php
+        $theParent = wp_get_post_parent_id(get_the_ID());
+        if ($theParent) {
+          ?>
+            <div class="metabox metabox--position-up metabox--with-home-link">
+              <p>
+                <a class="metabox__blog-home-link" href="<?php echo get_the_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent) ?></a> <span class="metabox__main"><?php echo the_title(); ?></span>
+              </p>
+            </div>
+            <?php
+        }
+      ?>
       <?php 
         $hasChildren = get_pages(array(
           'child_of' => get_the_ID()
@@ -50,13 +47,9 @@
           </ul>
         </div>
       <?php endif; ?>
-      
       <div class="generic-content">
         <?php the_content(); ?>
       </div>
-      
     </div>
   <?php endwhile; ?>
-<?php
- get_footer();
-?>
+<?php get_footer(); ?>

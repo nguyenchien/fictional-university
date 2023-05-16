219433e82227066e3bf5c1173f885bb7ca5dac71
chienvn <chiennguyen1702@gmail.com>
2023-05-16 21:33:09 +0700

    advance custom filed

--- front-page.php
+++ front-page.php
@@ -24,8 +24,13 @@
               $homeEvents->the_post();?>
               <div class="event-summary">
                 <a class="event-summary__date t-center" href="#">
-                  <span class="event-summary__month"><?php the_time('M'); ?></span>
-                  <span class="event-summary__day"><?php the_time('d'); ?></span>
+                  <?php
+                    $eventDay = new DateTime(get_field('event_day'));
+                    $month = $eventDay->format('M');
+                    $day = $eventDay->format('d');
+                  ?>
+                  <span class="event-summary__month"><?php echo $month; ?></span>
+                  <span class="event-summary__day"><?php echo $day; ?></span>
                 </a>
                 <div class="event-summary__content">
                   <h5 class="event-summary__title headline headline--tiny">

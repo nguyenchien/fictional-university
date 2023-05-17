923ff1b19651ec8c3878b4902c649364e1218ff0
chienvn <chiennguyen1702@gmail.com>
2023-05-18 05:41:57 +0700

    past events

--- page-past-events.php
+++ page-past-events.php
@@ -2,16 +2,34 @@
 <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
   <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title">All Events</h1>
+    <h1 class="page-banner__title">Past Events</h1>
     <div class="page-banner__intro">
-      <p>See all events on the word!</p>
+      <p>Events has been done!</p>
     </div>
   </div>
 </div>
 <div class="container container--narrow page-section">
   <?php
-    while(have_posts()) {
-      the_post();?>
+    $today = date('Ymd');
+    $pastEvents = new WP_Query(
+      array(
+        'paged' => get_query_var('paged', 1),
+        'post_type' => 'event',
+        'meta_key' => 'event_date',
+        'orderby' => 'meta_value_num',
+        'order' => 'ASC',
+        'meta_query' => array(
+          array(
+            'key' => 'event_date',
+            'value' => $today,
+            'type' => 'numeric',
+            'compare' => '<'
+          )
+        )
+      )
+    );
+    while($pastEvents->have_posts()) {
+      $pastEvents->the_post();?>
       <div class="event-summary">
         <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
           <?php 
@@ -33,10 +51,10 @@
   ?>
   <div class="pagination">
     <?php
-      echo paginate_links();
+      echo paginate_links(array(
+        'total' => $pastEvents->max_num_pages
+      ));
     ?>
   </div>
-  <hr class="section-break">
-  <p>Looking for a recap of past events. <a href="<?php echo site_url('past-events') ?>">Check it out past events archive.</a></p>
 </div>
 <?php get_footer(); ?>

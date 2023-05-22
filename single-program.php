4ef558e136682210f3cb8c890335440d29591caf
chienvn <chiennguyen1702@gmail.com>
2023-05-23 06:00:40 +0700

    show related event in progran

--- single-program.php
+++ single-program.php
@@ -14,12 +14,62 @@
       <div class="container container--narrow page-section">
         <div class="metabox metabox--position-up metabox--with-home-link">
           <p>
-            <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to Home Programs</a> <span class="metabox__main"><?php the_title(); ?></span>
+            <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Home Programs</a> <span class="metabox__main"><?php the_title(); ?></span>
           </p>
         </div>
         <div class="generic-content">
           <?php the_content(); ?>
         </div>
+        <?php
+            $today = date('Ymd');
+            $relatedEvents = new WP_Query(
+              array(
+                'post_type' => 'event',
+                'posts_per_page' => 2,
+                'meta_key' => 'event_date',
+                'orderby' => 'meta_value_num',
+                'order' => 'ASC',
+                'meta_query' => array(
+                  array(
+                    'key' => 'event_date',
+                    'value' => $today,
+                    'type' => 'numeric',
+                    'compare' => '>='
+                  ),
+                  array(
+                    'key' => 'related_programs',
+                    'value' => get_the_ID(),
+                    'compare' => 'LIKE'
+                  )
+                )
+              )
+            );
+            if ($relatedEvents->have_posts()) {
+            echo '<hr class="section-break">';
+            echo '<h2 class="headline headline--medium">Upcoming '. get_the_title() .' Event</h2>';
+            while($relatedEvents->have_posts()) {
+              $relatedEvents->the_post(); ?>
+              <div class="event-summary">
+                <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+                  <?php
+                    $eventDay = new DateTime(get_field('event_date'));
+                    $month = $eventDay->format('M');
+                    $day = $eventDay->format('d');
+                  ?>
+                  <span class="event-summary__month"><?php echo $month; ?></span>
+                  <span class="event-summary__day"><?php echo $day; ?></span>
+                </a>
+                <div class="event-summary__content">
+                  <h5 class="event-summary__title headline headline--tiny">
+                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                  </h5>
+                  <p><?php echo wp_trim_words(get_the_content(), 10); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                </div>
+              </div>
+            <?php }
+              wp_reset_postdata();
+            }
+          ?>
       </div>
       <?php
     }

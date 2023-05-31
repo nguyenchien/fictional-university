86cb68a0ed90f82d1ea31d98a1befb15d12f043c
nguyen.van.chien <nguyen.van.chien@m-hand.com>
2023-05-31 14:33:47 +0700

    post type professor

--- single-professor.php
+++ single-professor.php
@@ -14,7 +14,7 @@
       <div class="container container--narrow page-section">
         <div class="metabox metabox--position-up metabox--with-home-link">
           <p>
-            <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Home Events</a> <span class="metabox__main"><?php the_title(); ?></span>
+            <a class="metabox__blog-home-link" href="/"><i class="fa fa-home" aria-hidden="true"></i> Home</a> <span class="metabox__main"><?php the_title(); ?></span>
           </p>
         </div>
         <div class="generic-content">

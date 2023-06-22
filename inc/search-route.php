95fc0acf7b07ce8879d7df6f3f3e976d9fdc3635
nguyen.van.chien <nguyen.van.chien@m-hand.com>
2023-06-22 09:54:26 +0700

    rest api: create custom data respond

--- inc/search-route.php
+++ inc/search-route.php
@@ -1,11 +1,25 @@
 <?php
-add_action('rest_api_init', 'university_custom_route');
-function university_custom_route() {
-  register_rest_route( 'university/v1', '/search/', array(
-    'methods' => 'GET',
-    'callback' => 'universitySearchResult',
-  ));
-}
-function universitySearchResult() {
-  return 'hello 123';
-}
+  add_action('rest_api_init', 'university_custom_route');
+  function university_custom_route() {
+    register_rest_route( 'university/v1', '/search/', array(
+      'methods' => 'GET',
+      'callback' => 'universitySearchResult',
+    ));
+  }
+  function universitySearchResult() {
+    $professor = new WP_Query(
+      array (
+        'post_type' => 'professor'
+      )
+    );
+    $professorResult = array();
+    while($professor->have_posts()) {
+      $professor->the_post();
+      array_push($professorResult, array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink(),
+      ));
+    }
+    return $professorResult;
+  }
+?>

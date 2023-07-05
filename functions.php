cddaee10224571991ab2b1bb82b150a07fcf7c3f
nguyen.van.chien <nguyen.van.chien@m-hand.com>
2023-07-05 09:47:54 +0700

    security note

--- functions.php
+++ functions.php
@@ -143,6 +143,10 @@
   // make note to private
   add_filter('wp_insert_post_data', 'makeNotePrivate');
   function makeNotePrivate($data) {
+    if ($data['post_type'] == 'note') {
+      $data['post_title'] = sanitize_text_field($data['post_title']);
+      $data['post_content'] = sanitize_textarea_field($data['post_content']);
+    }
     if ($data['post_type'] == 'note' && $data['post_status'] != 'draft') {
       $data['post_status'] = "private";
     }
--- page-my-notes.php
+++ page-my-notes.php
@@ -23,10 +23,10 @@
           $myNotes->the_post();
       ?>
           <li data-id="<?php echo get_the_ID(); ?>">
-            <input class="note-title-field" readonly type="text" value="<?php echo esc_attr(get_the_title()); ?>">
+            <input class="note-title-field" readonly type="text" value="<?php echo str_replace('Private: ','',esc_attr(get_the_title())); ?>">
             <span class="edit-note"><i class="fa fa-pencil"></i> Edit</span>
             <span class="delete-note"><i class="fa fa-trash-o"></i> Delete</span>
-            <textarea class="note-body-field" readonly><?php echo esc_attr(wp_strip_all_tags(get_the_content())); ?></textarea>
+            <textarea class="note-body-field" readonly><?php echo esc_textarea(get_the_content()); ?></textarea>
             <span class="update-note btn btn--blue btn--small"><i class="fa fa-save"></i> Save</span>
           </li>
         <?php }

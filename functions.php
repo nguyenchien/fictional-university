520644ecc1698b22a8b68668e6598fc495271680
nguyen.van.chien <nguyen.van.chien@m-hand.com>
2023-07-11 09:41:11 +0700

    delete comment

--- functions.php
+++ functions.php
@@ -163,9 +163,6 @@
       $data['post_title'] = sanitize_text_field($data['post_title']);
       $data['post_content'] = sanitize_textarea_field($data['post_content']);
     }
-    // if ($data['post_type'] == 'note' && $data['post_status'] != 'draft' && $data['post_status'] == 'private') {
-    //   $data['post_status'] = "private";
-    // }
     return $data;
   }
 ?>
